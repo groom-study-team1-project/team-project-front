4efb3ba0cd4376fbc83f910a2405a0f9d4b5a5ac
shyeon0602 <july8560@naver.com>
2024-08-23 23:48:42 +0900

    Revert "feat(#12): AuthPage에 로그인, 회원가입 연결"
    This reverts commit 3bc6aaca15d82770bad2d82dbc2fa84aa4d8ec98.

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,22 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function AuthPage() {
   return (
     <>
-      <div>
-        AuthPage
-        <div>
-          <ul>
-            <li>
-              <Link to="/login">로그인</Link>
-            </li>
-            <li>
-              <Link to="/signup">회원가입</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <div>AuthPage</div>
     </>
   );
 }
