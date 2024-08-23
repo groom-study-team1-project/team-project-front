1cb2e6e2ef25ab5acefb979cde08c687f19b6ab8
shyeon0602 <july8560@naver.com>
2024-08-23 23:53:26 +0900

    feat(#12): 로그인, 회원가입 링크 생성

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AuthPage() {
   return (
     <>
-      <div>AuthPage</div>
+      <div>
+        AuthPage
+        <div>
+          <ul>
+            <li>
+              <Link to="/join">로그인</Link>
+            </li>
+            <li>
+              <Link to="/sign-up">회원가입</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
