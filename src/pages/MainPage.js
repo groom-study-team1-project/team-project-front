4419937126da94666c2495ac5e865ff02c4e5fc2
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2024-08-25 01:24:47 +0900

    feat(#7): 메인페이지 컨테이너 설정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1920px;
+  height: 95vh;
+  max-height: 100vh;
+  margin: 0 auto;
+  padding-top: 20px;
+  border: 1px solid black;
+`;
+
+const Header = styled.header`
+  padding: 20px;
+  text-align: center;
+  border: 1px solid black;
+`;
+
+const Content = styled.main`
+  padding: 20px;
+  margin-top: 155px;
+  text-align: center;
+  border: 1px solid black;
+`;
 
 function MainPage() {
   return (
     <>
-      <div>MainPage</div>
+      <Container>
+        <Header>네브바</Header>
+        <Content>Content</Content>
+      </Container>
     </>
   );
 }
