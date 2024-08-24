be2f81cb1a137ea99a1df7578073c276eb080ffe
shyeon0602 <july8560@naver.com>
2024-08-24 15:20:17 +0900

    feat(#12): 로그인, 회원가입 api 함수 생성

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+export const join = async (email, password) => {
+  // todo: 로그인 api 호출 함수 구현
+};
+
+export const signup = async (
+  profileImg,
+  name,
+  email,
+  password,
+  confirmPassword,
+  phoneNum
+) => {
+  // todo: 회원가입 api 호출 함수 구현(formData)
+};
