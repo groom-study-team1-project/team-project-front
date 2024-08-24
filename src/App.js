c862e86e0faf17648ec523d12d96628047a8921c
Seio924 <seio9241@gmail.com>
2024-08-24 19:50:18 +0900

    design(#8): styled components를 사용한 css reset으로 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Router from "./routes/Router";
+import GlobalStyle from "./assets/styles/GlobalStyle";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
