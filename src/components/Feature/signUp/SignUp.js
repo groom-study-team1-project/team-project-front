41e84f08e6f2f7a689ac97b9d42deb35d605bc7f
shyeon0602 <july8560@naver.com>
2024-08-24 16:44:47 +0900

    feat(#12): 회원가입 스타일 컴포넌트 적용

--- src/components/Feature/signUp/SignUp.js
+++ src/components/Feature/signUp/SignUp.js
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
-import styles from "./SignUp.module.css";
 import profileIcon from "../../../assets/images/profileIcon.png";
 import { signup } from "../../../services/api";
+import styled from "styled-components";
+
+const SignUpDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+`;
+const ProfileImgDiv = styled.div`
+  text-align: center;
+`;
+const Input = styled.input`
+  width: 400px;
+  height: 36px;
+`;
+const Btn = styled.button`
+  margin: 1rem 0;
+  width: 404px;
+  height: 40px;
+  cursor: pointer;
+`;
 
 export default function SignUp() {
   const [profileImg, setProfileImg] = useState(null);
@@ -40,41 +61,41 @@ export default function SignUp() {
   };
 
   return (
-    <div>
+    <SignUpDiv>
       <div>
         <h1>회원가입</h1>
       </div>
       <form onSubmit={handleSignUp}>
-        <div>
+        <ProfileImgDiv>
           <img src={profileImg ? profileImg : profileIcon} alt="프로필사진" />
           <div>
             <input type="file" onChange={handleImageChange} />
           </div>
-        </div>
+        </ProfileImgDiv>
         <div>
           <p>닉네임</p>
-          <input type="text" value={name} />
+          <Input type="text" value={name} />
         </div>
         <div>
           <p>이메일</p>
-          <input type="email" value={email} />
+          <Input type="email" value={email} />
         </div>
         <div>
           <p>비밀번호</p>
-          <input type="password" value={password} />
+          <Input type="password" value={password} />
         </div>
         <div>
           <p>비밀번호 확인</p>
-          <input type="password" value={confirmPassword} />
+          <Input type="password" value={confirmPassword} />
         </div>
         <div>
           <p>휴대폰 번호</p>
-          <input type="tel" value={phoneNum} />
+          <Input type="tel" value={phoneNum} />
         </div>
         <div>
-          <button type="submit">계정 생성하기</button>
+          <Btn type="submit">계정 생성하기</Btn>
         </div>
       </form>
-    </div>
+    </SignUpDiv>
   );
 }
