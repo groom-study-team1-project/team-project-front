f4cfb6fca27e847abd858137a5682160c79d79dd
shyeon0602 <july8560@naver.com>
2024-08-26 15:20:29 +0900

    feat(#12): 로그인, 회원가입 컴포넌트 div 생성 및 스타일 적용

--- src/components/Feature/login/Login.js
+++ src/components/Feature/login/Login.js
@@ -2,20 +2,33 @@ import React, { useState } from "react";
 import { join } from "../../../services/api";
 import styled from "styled-components";
 
-const LoginDiv = styled.div`
-  height: 90vh;
+const ContainerDiv = styled.div`
+  max-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
+`;
+const LoginDiv = styled.div`
+  height: 628px;
+  width: 530px;
+  margin: auto;
+  padding-top: 10rem;
+  box-sizing: border-box;
+`;
+const Logo = styled.div`
+  text-align: center;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const Input = styled.input`
   width: 400px;
   height: 36px;
   margin-bottom: ${(props) => props.marginBtm};
-`;
-const Btns = styled.div`
-  text-align: center;
 `;
 const Btn = styled.button`
   margin: 1rem 0;
@@ -42,29 +55,33 @@ export default function Login() {
   }
 
   return (
-    <LoginDiv>
-      {/* Login */}
-      <div className="logo">
-        <h1>로고 이미지</h1>
-      </div>
-      <form action="" method="post" onSubmit={handleLogin}>
-        <div className="email">
-          <p>이메일</p>
-          <Input type="email" value={email} />
-        </div>
-        <div className="password">
-          <p>비밀번호</p>
-          <Input type="password" value={password} marginBtm="1rem" />
-        </div>
-        <Btns>
-          <div className="loginBtn">
-            <Btn type="submit">로그인</Btn>
+    <ContainerDiv>
+      <LoginDiv>
+        {/* Login */}
+        <Logo>
+          <h1>로고 이미지</h1>
+        </Logo>
+        <Form action="" method="post" onSubmit={handleLogin}>
+          <div className="email">
+            <p>이메일</p>
+            <Input type="email" value={email} />
           </div>
-          <div className="signUpBtn">
-            <Btn>회원가입</Btn>
+          <div className="password">
+            <p>비밀번호</p>
+            <Input type="password" value={password} marginBtm="1rem" />
           </div>
-        </Btns>
-      </form>
-    </LoginDiv>
+          <div className="btns">
+            <div style={{ borderBottom: "1px solid darkgray" }}>
+              <Btn type="submit" id="loginBtn">
+                로그인
+              </Btn>
+            </div>
+            <div>
+              <Btn id="signUpBtn">회원가입</Btn>
+            </div>
+          </div>
+        </Form>
+      </LoginDiv>
+    </ContainerDiv>
   );
 }
--- src/components/Feature/signUp/SignUp.js
+++ src/components/Feature/signUp/SignUp.js
@@ -3,12 +3,30 @@ import profileIcon from "../../../assets/images/profileIcon.png";
 import { signup } from "../../../services/api";
 import styled from "styled-components";
 
-const SignUpDiv = styled.div`
+const ContainerDiv = styled.div`
+  max-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 90vh;
+  overflow: hidden;
+`;
+const SignUpDiv = styled.div`
+  height: 918px;
+  width: 530px;
+  background-color: rgba(255, 255, 255, 0.5);
+  margin: auto;
+  padding-top: 5rem;
+  box-sizing: border-box;
+`;
+const Logo = styled.div`
+  text-align: left;
+  margin-left: 4rem;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const ProfileImgDiv = styled.div`
   text-align: center;
@@ -61,41 +79,43 @@ export default function SignUp() {
   };
 
   return (
-    <SignUpDiv>
-      <div>
-        <h1>회원가입</h1>
-      </div>
-      <form onSubmit={handleSignUp}>
-        <ProfileImgDiv>
-          <img src={profileImg ? profileImg : profileIcon} alt="프로필사진" />
+    <ContainerDiv>
+      <SignUpDiv>
+        <Logo>
+          <h1>회원가입</h1>
+        </Logo>
+        <Form onSubmit={handleSignUp}>
+          <ProfileImgDiv>
+            <img src={profileImg ? profileImg : profileIcon} alt="프로필사진" />
+            <div>
+              <input type="file" onChange={handleImageChange} />
+            </div>
+          </ProfileImgDiv>
           <div>
-            <input type="file" onChange={handleImageChange} />
+            <p>닉네임</p>
+            <Input type="text" value={name} />
           </div>
-        </ProfileImgDiv>
-        <div>
-          <p>닉네임</p>
-          <Input type="text" value={name} />
-        </div>
-        <div>
-          <p>이메일</p>
-          <Input type="email" value={email} />
-        </div>
-        <div>
-          <p>비밀번호</p>
-          <Input type="password" value={password} />
-        </div>
-        <div>
-          <p>비밀번호 확인</p>
-          <Input type="password" value={confirmPassword} />
-        </div>
-        <div>
-          <p>휴대폰 번호</p>
-          <Input type="tel" value={phoneNum} />
-        </div>
-        <div>
-          <Btn type="submit">계정 생성하기</Btn>
-        </div>
-      </form>
-    </SignUpDiv>
+          <div>
+            <p>이메일</p>
+            <Input type="email" value={email} />
+          </div>
+          <div>
+            <p>비밀번호</p>
+            <Input type="password" value={password} />
+          </div>
+          <div>
+            <p>비밀번호 확인</p>
+            <Input type="password" value={confirmPassword} />
+          </div>
+          <div>
+            <p>휴대폰 번호</p>
+            <Input type="tel" value={phoneNum} />
+          </div>
+          <div style={{ borderTop: "1px solid darkgray", marginTop: "16px" }}>
+            <Btn type="submit">계정 생성하기</Btn>
+          </div>
+        </Form>
+      </SignUpDiv>
+    </ContainerDiv>
   );
 }
