ca064a7eeb4d002da47471e04b88b779fa37a467
shyeon0602 <july8560@naver.com>
2024-08-27 17:41:56 +0900

    design(#19): 사이드바 border 추가

--- src/components/Common/SideBar.js
+++ src/components/Common/SideBar.js
@@ -2,34 +2,46 @@ import React, { useEffect, useState } from "react";
 import { fetchSideBarMenuItems } from "../../services/api";
 import styled from "styled-components";
 
-export default function SideBar() {
-  const ContainerDiv = styled.div`
-    max-height: 1080px;
-    height: 90vh;
-    width: 314px;
-  `;
-  const SideBarDiv = styled.div`
-    height: 296px;
-    margin-top: 8rem;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  `;
-  const Logo = styled.div`
-    margin: auto;
-    margin-left: 1rem;
-    box-sizing: border-box;
-  `;
-  const SideBarUl = styled.ul`
-    list-style-type: none;
-    padding-left: 2rem;
-  `;
-  const SideBarLi = styled.li`
-    margin-bottom: 2rem;
-  `;
+const ContainerDiv = styled.div`
+  max-height: 1080px;
+  height: 90vh;
+  width: 314px;
+  // container border는 원래 없는 거라서 스켈레톤에서만 보임
+  border-right: 1px solid gray;
+`;
 
+const SideBarDiv = styled.div`
+  margin-top: 8rem;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Logo = styled.div`
+  margin: 1rem;
+  box-sizing: border-box;
+`;
+
+const SideBarUl = styled.ul`
+  list-style-type: none;
+  padding-left: 2rem;
+  padding-right: 8px;
+`;
+
+const SideBarLi = styled.li`
+  margin-bottom: 1rem;
+  padding: 1rem 0;
+  border: 1px solid black;
+`;
+
+const SideBarLink = styled.a`
+  text-decoration: none;
+  color: black;
+`;
+
+export default function SideBar() {
   const [menuItems, setMenuItems] = useState([]);
 
   useEffect(() => {
@@ -39,6 +51,7 @@ export default function SideBar() {
         setMenuItems(items);
       } catch (error) {
         console.error(error);
+        // 사용자에게 오류 메시지 표시를 고려할 수 있습니다.
       }
     };
 
@@ -47,7 +60,6 @@ export default function SideBar() {
 
   return (
     <ContainerDiv>
-      {/* SideBar */}
       <Logo>
         <h2>로고이미지</h2>
       </Logo>
@@ -55,12 +67,7 @@ export default function SideBar() {
         <SideBarUl>
           {menuItems.map((item) => (
             <SideBarLi key={item.id}>
-              <a
-                href={item.link}
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                {item.item}
-              </a>
+              <SideBarLink href={item.link}>{item.item}</SideBarLink>
             </SideBarLi>
           ))}
         </SideBarUl>
