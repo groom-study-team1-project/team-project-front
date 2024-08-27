72972af0d89cf63f330c8b0c7ce1b9ae0771bf91
HYJIN\isacc <isacc7224@gmail.com>
2024-08-27 20:42:38 +0900

    feat(#16): 글 상세 페이지 구조 작성

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,9 +1,72 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
+const Wrap = styled.div`
+  width: 1028px;
+  margin: auto auto;
+  font-size: 16px;
+`;
+
+const CategotyWrap = styled.div`
+  width: 260px;
+  height: 54px;
+  border: 1px solid black;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  border-radius: 20px;
+`;
+
+const Profile = styled.div`
+  display: flex;
+`;
+
+const ProfileImg = styled.img`
+  border-radius: 50%;
+`;
 function DetailPage() {
   return (
     <>
-      <div>DetailPage</div>
+      <Wrap>
+        <CategotyWrap>게시판 이름</CategotyWrap>
+        <div>
+          <Profile>
+            <ProfileImg
+              src={require("../assets/images/profileIcon.png")}
+              alt="프로필 이미지"
+            />
+            <div>
+              <div>작성자 이름</div>
+              <div>작성자 소개</div>
+            </div>
+          </Profile>
+          <div>이미지 슬라이더</div>
+          <CKEditor
+            editor={ClassicEditor}
+            data={"<p>Hello World</p>"}
+            disabled="true"
+          />
+        </div>
+        <div>
+          <div>
+            <img src={require("../assets/images/eye.png")} alt="조회수" />
+            12
+          </div>
+          <div>
+            <img src={require("../assets/images/heart.png")} alt="좋아요" />
+            12
+          </div>
+          <div>
+            <img src={require("../assets/images/comment.png")} alt="댓글수" />
+            12
+          </div>
+        </div>
+        <div>
+          <div>댓글</div>
+        </div>
+      </Wrap>
     </>
   );
 }
