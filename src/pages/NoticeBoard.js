164e1c4b373fae4b69569bde48efe5554f938aff
Seio924 <seio9241@gmail.com>
2024-08-28 11:08:59 +0900

    feat(#10): 게시글 카드 기본 구조 설정 및 API 호출 구조 추가

--- src/pages/NoticeBoard.js
+++ src/pages/NoticeBoard.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchNoticePostItems } from "../services/api";
+import NoticePostCard from "../components/Feature/NoticePostCard";
 
 function NoticeBoard() {
+  const [postItems, setPostItems] = useState([]);
+
+  useEffect(() => {
+    fetchNoticePostItems()
+      .then((postItems) => setPostItems(postItems))
+      .catch((err) => console.log(err));
+  });
+
   return (
     <>
-      <div>NoticeBoard</div>
+      {postItems.map((post) => (
+        <NoticePostCard
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          date={post.date}
+          count={post.count}
+        />
+      ))}
     </>
   );
 }
