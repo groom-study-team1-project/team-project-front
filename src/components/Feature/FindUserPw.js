c77e4f4a537ee9599b10698ecd31cbd06ab7634a
shyeon0602 <july8560@naver.com>
2024-08-28 17:08:10 +0900

    refactor(#13): 분리됐던 스타일 컴포넌트 합체

--- src/components/Feature/FindUserPw.js
+++ src/components/Feature/FindUserPw.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { findUserPw } from "../../services/api";
-import {
-  ContainerDiv,
-  Logo,
-  Form,
-  Input,
-  Btn,
-} from "../../assets/styles/AuthCommonStyles";
+
+const ContainerDiv = styled.div`
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
 
 const FindUserPwDiv = styled.div`
   height: 628px;
@@ -16,6 +18,31 @@ const FindUserPwDiv = styled.div`
   margin-top: 8rem;
   padding-top: 10rem;
   box-sizing: border-box;
+`;
+
+const Logo = styled.div`
+  text-align: left;
+  margin-left: 4rem;
+  margin-bottom: 4rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 400px;
+  height: 36px;
+  margin-bottom: ${(props) => props.marginBtm};
+`;
+
+const Btn = styled.button`
+  margin: 1rem 0;
+  width: 404px;
+  height: 40px;
+  cursor: pointer;
 `;
 
 export default function FindUserPw() {
