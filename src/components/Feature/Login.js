140ccd3d1fd2094b9e1becd55fe90e208a887fad
shyeon0602 <july8560@naver.com>
2024-08-28 09:31:39 +0900

    refactor(#13): 로그인 스타일 컴포넌트 분리

--- src/components/Feature/Login.js
+++ src/components/Feature/Login.js
@@ -2,41 +2,23 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { join } from "../../services/api";
 import styled from "styled-components";
+import {
+  ContainerDiv,
+  Logo,
+  Form,
+  Input,
+  Btn,
+} from "../../assets/styles/AuthCommonStyles";
 
-const ContainerDiv = styled.div`
-  max-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-`;
 const LoginDiv = styled.div`
   height: 628px;
   width: 530px;
   margin: auto;
+  margin-top: 8rem;
   padding-top: 10rem;
   box-sizing: border-box;
 `;
-const Logo = styled.div`
-  text-align: center;
-`;
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const Input = styled.input`
-  width: 400px;
-  height: 36px;
-  margin-bottom: ${(props) => props.marginBtm};
-`;
-const Btn = styled.button`
-  margin: 1rem 0;
-  width: 404px;
-  height: 40px;
-  cursor: pointer;
-`;
+
 const FindUserBtn = styled.button`
   border: none;
   background: none;
