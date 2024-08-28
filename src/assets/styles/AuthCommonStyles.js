880f7291993247e009214c9c52bf2367b612837e
shyeon0602 <july8560@naver.com>
2024-08-28 09:20:28 +0900

    refactor(#13): 공통 스타일 컴포넌트 분리

---
+++ src/assets/styles/AuthCommonStyles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ContainerDiv = styled.div`
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+export const Logo = styled.div`
+  text-align: left;
+  margin-left: 4rem;
+  margin-bottom: 4rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 400px;
+  height: 36px;
+  margin-bottom: ${(props) => props.marginBtm};
+`;
+
+export const Btn = styled.button`
+  margin: 1rem 0;
+  width: 404px;
+  height: 40px;
+  cursor: pointer;
+`;
