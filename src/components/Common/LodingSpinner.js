577a5a8416b0dbd830190caf822da87ca3909cf2
HYJIN\isacc <isacc7224@gmail.com>
2024-08-29 17:40:10 +0900

    feat(#17): 로딩spinner 컴포넌트 구현

---
+++ src/components/Common/LodingSpinner.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { PulseLoader, DotLoader, BarLoader } from "react-spinners";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+export const PulseLoading = () => {
+  return (
+    <>
+      <Wrap>
+        <PulseLoader size={20} />
+      </Wrap>
+    </>
+  );
+};
+
+export const DotLoading = () => {
+  return (
+    <>
+      <Wrap>
+        <DotLoader size={100} />
+      </Wrap>
+    </>
+  );
+};
+
+export const BarLoading = () => {
+  return (
+    <>
+      <Wrap>
+        <BarLoader size={100} />
+      </Wrap>
+    </>
+  );
+};
