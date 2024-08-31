69a699691c0474bdb1631041b03de10e9518db17
아윤봄 <jbomi0212@gmail.com>
2024-08-31 15:29:51 +0900

    feat(#15): 게시판별 PostCard 컴포넌트 동적 렌더링 & 검색/정렬 위치 조정

--- src/components/Layout/BoardLayout.js
+++ src/components/Layout/BoardLayout.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import NavBar from "../Common/Navbar";
-import Sidebar from "../Common/SideBar";
+import SideBar from "../Common/SideBar";
 import PopularPostCard from "../Common/PopularPostCard";
 import PopularHashCard from "../Common/PopularHashCard";
+import ProjectPostCard from "../Common/ProjectPostCard";
+import CommunityPostCard from "../Common/CommunityPostCard";
+import NoticePostCard from "../Feature/NoticePostCard";
 
 const Container = styled.div`
   display: flex;
@@ -13,28 +17,66 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const Navbar = styled(NavBar)`
+const StyledNavbar = styled(NavBar)`
   z-index: 1;
 `;
 
-const MainContainer = styled.div`
+const MainContentWrapper = styled.div`
   width: 100%;
   display: flex;
   flex: 1;
   z-index: 2;
 `;
 
-const LeftSideBar = styled(Sidebar)`
-  position: fixed;
-`;
-
-const Content = styled.div`
+const ContentArea = styled.div`
+  width: 100%;
   flex: 1;
-  background-color: white;
-  border: 2px solid red;
 `;
 
-const RightSideBar = styled.div`
+const BoardTitle = styled.div`
+  width: ${(props) => (props.text === "프로젝트 게시판" ? "264px" : "200px")};
+  height: 48px;
+  display: flex;
+  border: 1px solid black;
+  border-radius: 20px;
+  margin-top: 145px;
+  margin-left: 24px;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const SearchSortWrapper = styled.div`
+  width: auto;
+  height: 32px;
+  display: flex;
+  margin-top: 50px;
+  padding-left: 48px;
+  border: 1px solid black;
+`;
+
+const SearchBox = styled.div`
+  width: 378px;
+  height: 32px;
+  border: 1px solid blue;
+`;
+
+const SortOption = styled.div`
+  width: 80px;
+  height: 24px;
+  margin-left: auto;
+  border: 1px solid blue;
+  align-self: flex-end;
+`;
+
+const ContentWrapper = styled.div`
+  width: auto;
+  height: auto;
+  padding-left: 80px;
+  padding-top: 40px;
+`;
+const RightSidebarWrapper = styled.div`
   width: auto;
   display: flex;
   flex-direction: column;
@@ -42,26 +84,67 @@ const RightSideBar = styled.div`
   margin-right: 94px;
 `;
 
-const Popularcard = styled.div`
+const CardWrapper = styled.div`
   margin-bottom: 40px;
 `;
 
 function BoardLayout() {
+  const location = useLocation();
+
+  const determineBoardTitle = () => {
+    switch (location.pathname) {
+      case "/community/projects":
+        return "프로젝트 게시판";
+      case "/community/notices":
+        return "공지사항";
+      case "/community/free":
+        return "자유게시판";
+      case "/community/questions":
+        return "질문게시판";
+      default:
+        return null;
+    }
+  };
+
+  const renderPostCard = () => {
+    switch (location.pathname) {
+      case "/community/projects":
+        return <ProjectPostCard />;
+      case "/community/notices":
+        return <NoticePostCard />;
+      default:
+        if (location.pathname.startsWith("/community")) {
+          return <CommunityPostCard />;
+        }
+        return null;
+    }
+  };
+
   return (
     <Container>
-      <Navbar />
-      <MainContainer>
-        <LeftSideBar />
-        <Content>Content</Content>
-        <RightSideBar>
-          <Popularcard>
+      <StyledNavbar isMainPage={false} />
+      <MainContentWrapper>
+        <SideBar />
+        <ContentArea>
+          <BoardTitle text={determineBoardTitle()}>
+            {determineBoardTitle()}
+          </BoardTitle>
+          <SearchSortWrapper>
+            <SearchBox>Search</SearchBox>
+            <SortOption>최신순</SortOption>
+            <ContentWrapper></ContentWrapper>
+          </SearchSortWrapper>
+          <ContentWrapper>{renderPostCard()}</ContentWrapper>
+        </ContentArea>
+        <RightSidebarWrapper>
+          <CardWrapper>
             <PopularPostCard />
-          </Popularcard>
-          <Popularcard>
+          </CardWrapper>
+          <CardWrapper>
             <PopularHashCard />
-          </Popularcard>
-        </RightSideBar>
-      </MainContainer>
+          </CardWrapper>
+        </RightSidebarWrapper>
+      </MainContentWrapper>
     </Container>
   );
 }
