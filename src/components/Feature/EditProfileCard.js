2092809293958227948a376ecefcfb61f3970083
Seio924 <seio9241@gmail.com>
2024-08-31 17:43:53 +0900

    feat(#42): 마이페이지에 들어갈 요소 추가

--- src/components/Feature/EditProfileCard.js
+++ src/components/Feature/EditProfileCard.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { PostProfile } from "../Common/PostCardComponents";
+import React, { useState } from "react";
+import { ProfileImage } from "../Common/PostCardComponents";
+import { Form, FormInputField } from "../Common/AuthCommonComponents";
+import styled from "styled-components";
 
-function EditProfileCard({ name, job }) {
+const Btn = styled.button``;
+
+function EditProfileCard() {
+  const [nickname, setNickname] = useState("");
+  const [job, setJob] = useState("");
+  const [selfIntro, setSelfIntro] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function handleEdit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <PostProfile name={name} job={job} />
+      <ProfileImage />
+      <Form action="" method="post" onSubmit={handleEdit}>
+        <FormInputField label={"닉네임"} type={"text"} value={nickname} />
+        <FormInputField label={"직업"} type={"text"} value={job} />
+        <FormInputField label={"자기소개"} type={"text"} value={selfIntro} />
+        <FormInputField label={"이메일"} type={"email"} value={email} />
+
+        <Btn type="submit">수정</Btn>
+        <Btn>취소</Btn>
+      </Form>
     </>
   );
 }
--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -3,20 +3,9 @@ import { fetchPostItems } from "../services/api";
 import EditProfileCard from "../components/Feature/EditProfileCard";
 
 function EditProfile() {
-  const [postItems, setPostItems] = useState([]);
-
-  useEffect(() => {
-    fetchPostItems()
-      .then((postItems) => {
-        console.log("Fetched Post Items:", postItems[0]);
-        setPostItems(postItems);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <>
-      <EditProfileCard name={postItems[0]?.name} job={postItems[0]?.job} />
+      <EditProfileCard />
     </>
   );
 }
