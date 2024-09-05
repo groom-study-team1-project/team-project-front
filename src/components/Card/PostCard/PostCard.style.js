f023dd5fb13e9b27e423734d9c4c2d919694f8cd
Seio924 <seio9241@gmail.com>
2024-09-05 09:22:54 +0900

    refactor(#51): css파일 분할

--- src/components/Card/PostCard/PostCard.style.js
+++ src/components/Card/PostCard/PostCard.style.js
@@ -13,3 +13,30 @@ export const Thumbnail = styled.div`
   flex-grow: 1;
   border: 1px solid;
 `;
+
+export const PostCardWrapper = styled.div`
+  display: flex;
+  border: 1px solid;
+  width: ${({ width }) => width || "100%"};
+  height: ${({ height }) => height || "100%"};
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: ${({ direction }) => direction || "row"};
+  flex-grow: 1;
+  margin: 10px;
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 10px 0 10px;
+`;
+
+export const PostActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
