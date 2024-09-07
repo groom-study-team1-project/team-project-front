ede7fa8e3d61703a97cb83c2e9a7aac06a63ce3d
Seio924 <seio9241@gmail.com>
2024-09-07 11:02:42 +0900

    feat(#57): 로그인 모달 기능 추가

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchMenuItems } from "../../services/api";
 import {
   Logo,
@@ -9,15 +9,28 @@ import {
   MenuItem,
   Button,
 } from "./Navbar.style";
+import Modal from "react-modal";
+import LoginModal from "../../components/Modal/LoginModal/LoginModal";
+
+Modal.setAppElement("#root");
 
 function Navbar({ isMainPage = true, isLoggedIn = true }) {
   const [menuItems, setMenuItems] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     fetchMenuItems()
       .then((menuItems) => setMenuItems(menuItems))
       .catch((err) => console.log(err.message));
   }, []);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <NavbarWrapper>
@@ -35,9 +48,28 @@ function Navbar({ isMainPage = true, isLoggedIn = true }) {
         {isLoggedIn ? (
           <Button>글쓰기 다크모드 프로필</Button>
         ) : (
-          <Button>다크모드 로그인 회원가입</Button>
+          <Button onClick={openModal}>다크모드 로그인 회원가입</Button>
         )}
       </NavbarInner>
+
+      {/* react-modal 모달 */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="로그인 모달"
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+      >
+        <LoginModal />
+      </Modal>
     </NavbarWrapper>
   );
 }
