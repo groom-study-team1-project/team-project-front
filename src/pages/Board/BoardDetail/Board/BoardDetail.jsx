1674aae1c8048a49e0d6d37872d11944aa745f96
Re-Note <dldydtn207@gmail.com>
2025-01-21 06:18:55 +0900

    feat(#229): 조회수 증가 오류 해결

--- src/pages/Board/BoardDetail/Board/BoardDetail.jsx
+++ src/pages/Board/BoardDetail/Board/BoardDetail.jsx
@@ -1,15 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import { DecoupledEditor } from "ckeditor5";
 import Slide from "../../../../components/Common/imgSlide";
 import { editorConfig } from "../../BoardWrite/WriteBoard/editor";
-import {
-  fetchPostDetail,
-  fetchProjectPostDetail,
-  deletepost,
-  deleteProjectPost,
-} from "../../../../services/api/postApi";
+import {fetchPostDetail, fetchProjectPostDetail, deletepost, deleteProjectPost} from "../../../../services/api/postApi";
 import { fetchProfileInfo } from "../../../../services/api/authApi";
 import { PostProfileBox } from "../../../../components/Card/PostCard/PostProfile";
 import { Interaction } from "../../../../components/Common/Interactions";
@@ -34,35 +29,35 @@ function BoardDetail() {
   const [confirmModalVisible, setConfirmModalVisible] = useState(false);
   const { isMobile } = useSelector((state) => state.screenSize);
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const selectedCategoryId = useSelector(
-    (state) => state.category.selectedCategoryId
-  );
+  const selectedCategoryId = useSelector((state) => state.category.selectedCategoryId);
   const [isMe, setIsMe] = useState(false);
   const navigate = useNavigate();
   const { postId } = useParams();
-  const payload = useJwt(
-    useSelector((state) => state.user.userInfo.accessToken)
-  );
+  const payload = useSelector((state) => state.user.userInfo.accessToken);
+  const memberId = useMemo(() => payload?.memberId, [payload]);
+  const isDarkMode = useSelector((state) => state.theme.isDarkMode);
 
   useEffect(() => {
+    console.log("Fetching data for postId:", postId);
+
+    let isCancelled = false;
+
     const fetchData = async () => {
       try {
-        let postResponse;
+        const fetchDetail = selectedCategoryId === 2 ? fetchProjectPostDetail : fetchPostDetail;
+        const postResponse = await fetchDetail(postId);
 
-        if (selectedCategoryId === 2) {
-          postResponse = await fetchProjectPostDetail(postId);
-        } else {
-          postResponse = await fetchPostDetail(postId);
-        }
-        setPost(postResponse);
+        if (!isCancelled) {
+          setPost(postResponse);
 
-        if (isLoggedIn && postResponse) {
-          const body = {
-            isMe: payload.memberId,
-            memberId: postResponse.authorInformation.memberId,
-          };
-          const { isMe } = await fetchProfileInfo(body);
-          setIsMe(isMe);
+          if (isLoggedIn && postResponse) {
+            const body = {
+              isMe: memberId,
+              memberId: postResponse.authorInformation.memberId,
+            };
+            const { isMe } = await fetchProfileInfo(body);
+            setIsMe(isMe);
+          }
         }
       } catch (error) {
         console.error("데이터를 가져오는데 실패", error);
@@ -70,26 +65,24 @@ function BoardDetail() {
     };
 
     fetchData();
-  }, [postId, selectedCategoryId, isLoggedIn, payload]);
+
+    return () => {
+      isCancelled = true;
+    };
+  }, [postId, selectedCategoryId, isLoggedIn, memberId]);
 
   if (!post) {
     return <div>Loading...</div>;
   }
 
   const handleEdit = () => {
-    navigate(`/board/edit/${postId}`, {
-      state: { postData: post, categoryId: selectedCategoryId },
-    });
-    console.log("sadd", post);
+    navigate(`/board/edit/${postId}`, { state: { postData: post, categoryId: selectedCategoryId } });
   };
 
   const handleDelete = async () => {
     try {
-      if (selectedCategoryId === 2) {
-        await deleteProjectPost(postId);
-      } else {
-        await deletepost(postId);
-      }
+      const deleteFunc = selectedCategoryId === 2 ? deleteProjectPost : deletepost;
+      await deleteFunc(postId);
       setConfirmModalVisible(false);
       navigate(-1);
     } catch (error) {
@@ -98,74 +91,75 @@ function BoardDetail() {
   };
 
   return (
-    <>
-      <ContentWrapper $isDetail={true}>
-        <Wrap>
-          <PostWrap>
-            <Title $isMobile={isMobile}>{post.title}</Title>
-            <Postheader $isMobile={isMobile}>
-              <PostProfileBox
-                name={post.authorInformation.nickname}
-                job={post.authorInformation.memberJob}
-                email={post.authorInformation.email}
-                imgUrl={post.authorInformation.imageUrl}
-              />
-              <PostheaderRignt $isMobile={isMobile}>
-                {isMe && (
-                  <>
-                    <Modify onClick={handleEdit}>수정</Modify>
-                    <Modify onClick={() => setConfirmModalVisible(true)}>
-                      삭제
-                    </Modify>
-                  </>
+      <>
+        <ContentWrapper $isDetail={true}>
+          <Wrap>
+            <PostWrap isDarkMode={isDarkMode}>
+              <Title $isMobile={isMobile}>{post.title}</Title>
+              <Postheader $isMobile={isMobile}>
+                <PostProfileBox
+                    name={post.authorInformation.nickname}
+                    job={post.authorInformation.memberJob}
+                    email={post.authorInformation.email}
+                    imgUrl={post.authorInformation.imageUrl}
+                />
+                <PostheaderRignt $isMobile={isMobile}>
+                  {isMe && (
+                      <>
+                        <Modify onClick={handleEdit}>수정</Modify>
+                        <Modify onClick={() => setConfirmModalVisible(true)}>
+                          삭제
+                        </Modify>
+                      </>
+                  )}
+                </PostheaderRignt>
+              </Postheader>
+              {selectedCategoryId === 2 &&
+                  post.slideImageUrls?.length > 0 && (
+                      <div style={{ marginBottom: "20px" }}>
+                        <Slide imgUrls={post.slideImageUrls} />
+                      </div>
+                  )}
+              <ContentWrap>
+                <CKEditor
+                    editor={DecoupledEditor}
+                    config={editorConfig}
+                    data={post.content}
+                    disabled={true}
+                />
+              </ContentWrap>
+            </PostWrap>
+            <PostFooter $isMobile={isMobile}>
+              <div>
+                {post.hashtags && post.hashtags.length > 0 ? (
+                    post.hashtags.map((hashtag, index) => (
+                        <span key={index}>{hashtag}</span>
+                    ))
+                ) : (
+                    <span>#</span>
                 )}
-              </PostheaderRignt>
-            </Postheader>
-            {selectedCategoryId === 2 && post.slideImageUrls?.length > 0 && (
-              <div style={{ marginBottom: "20px" }}>
-                <Slide imgUrls={post.slideImageUrls} />
               </div>
-            )}
-            <ContentWrap>
-              <CKEditor
-                editor={DecoupledEditor}
-                config={editorConfig}
-                data={post.content}
-                disabled={true}
+              <Interaction
+                  count={{
+                    viewCount: post.viewCount,
+                    likeCount: post.likeCount,
+                    commentCount: post.commentCount,
+                  }}
               />
-            </ContentWrap>
-          </PostWrap>
-          <PostFooter $isMobile={isMobile}>
-            <div>
-              {post.hashtags && post.hashtags.length > 0 ? (
-                post.hashtags.map((hashtag, index) => (
-                  <span key={index}>{hashtag}</span>
-                ))
-              ) : (
-                <span>#</span>
-              )}
-            </div>
-            <Interaction
-              count={{
-                viewCount: post.viewCount,
-                likeCount: post.likeCount,
-                commentCount: post.commentCount,
-              }}
-            />
-          </PostFooter>
-          <Comments />
-        </Wrap>
-      </ContentWrapper>
-      <ConfirmDeleteModal
-        isVisible={confirmModalVisible}
-        onClose={() => setConfirmModalVisible(false)}
-        onConfirm={handleDelete}
-        title="삭제하시겠습니까?"
-        description="이 작업은 되돌릴 수 없습니다."
-        confirmText="확인"
-        cancelText="취소"
-      />
-    </>
+            </PostFooter>
+            <Comments />
+          </Wrap>
+        </ContentWrapper>
+        <ConfirmDeleteModal
+            isVisible={confirmModalVisible}
+            onClose={() => setConfirmModalVisible(false)}
+            onConfirm={handleDelete}
+            title="삭제하시겠습니까?"
+            description="이 작업은 되돌릴 수 없습니다."
+            confirmText="확인"
+            cancelText="취소"
+        />
+      </>
   );
 }
 
