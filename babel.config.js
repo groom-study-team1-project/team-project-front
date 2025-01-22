97ade61910f53a0d196af1538c6907a43011e072
HYJIN\isacc <isacc7224@gmail.com>
2025-01-22 10:51:29 +0900

    fix(#main): console 삭제

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    process.env.NODE_ENV === "production"
-      ? ["transform-remove-console", { exclude: [] }] // 모든 console 제거
-      : null,
-  ].filter(Boolean),
-};
